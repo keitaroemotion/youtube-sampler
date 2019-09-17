4b769bd01b64cb2e158e5f57dec37ac981e3b1eb
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-17 23:20:00 +0900

    youtube installer set

--- src/sampler/sampler.go
+++ src/sampler/sampler.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+    "log"
+    "os/user"
+    "path/filepath"
+    . "github.com/kkdai/youtube"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+    flag.Parse()
+    log.Println(flag.Args())
+    
+    usr, _     := user.Current()
+    currentDir := fmt.Sprintf("%v/Movies/youtubedr", usr.HomeDir)
+
+    log.Println("download to dir=", currentDir)
+    y      := NewYoutube(true)
+    arg    := flag.Arg(0)
+
+    if err := y.DecodeURL(arg); err != nil {
+        fmt.Println("err:", err)
+    }
+
+    if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
+        fmt.Println("err:", err)
+    }
 }
