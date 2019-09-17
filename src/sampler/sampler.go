2b2abbf9e5daedccfb8560b9e023a5262b00b4af
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-17 23:27:25 +0900

    validation added

--- src/sampler/sampler.go
+++ src/sampler/sampler.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "fmt"
     "log"
+    "os"
     "os/user"
     "path/filepath"
     . "github.com/kkdai/youtube"
@@ -11,19 +12,22 @@ import (
 
 func main() {
     flag.Parse()
+
+    if len(os.Args) == 1 {
+        fmt.Println("input argument missing.\n")
+        os.Exit(3) 
+    }
+
     log.Println(flag.Args())
     
     usr, _     := user.Current()
     currentDir := fmt.Sprintf("%v/Movies/youtubedr", usr.HomeDir)
 
     log.Println("download to dir=", currentDir)
-    y      := NewYoutube(true)
-    arg    := flag.Arg(0)
+    y          := NewYoutube(true)
+    arg        := flag.Arg(0)
 
-    if err := y.DecodeURL(arg); err != nil {
-        fmt.Println("err:", err)
-    }
-
+    if err := y.DecodeURL(arg); err != nil { fmt.Println("err:", err) }
     if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
         fmt.Println("err:", err)
     }
